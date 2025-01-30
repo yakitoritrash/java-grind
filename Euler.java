dced3a72cf662507dbff1b60773dfd890f2d4335
Kushagra Dwivedi <kishudwivedi30@gmail.com>
2025-01-31 02:59:44 +0530

    euler

--- Euler.java
+++ Euler.java
@@ -3,7 +3,14 @@ public class Euler{
 		if (n == 1) return 1;
 		int result = n;
 
-		if ()
+		if (n % 2 == 0) {
+			result -= result/2;
+			while (n % 2 == 0) n /= 2;
+		}
+
+		for (int p =3; p*p <=n; p+=2) {
+			if(n % p == 0)
+		}
 	
 	}
 
