13ffcf8fd128da0c01d14b794e3155b3b97be11b
Kushagra Dwivedi <kishudwivedi30@gmail.com>
2025-01-31 02:08:06 +0530

    simple sieve

--- Sieve.java
+++ Sieve.java
@@ -7,6 +7,16 @@ public class Sieve {
 		for (int i =2; i<=limit; i++) {
 			prime[i] = true;
 		}
+
+		for (int p = 2; p * p <= limit; p++){
+			if(prime[p] == true) {
+				for (int i = p*p; i <= limit; i+=p) {
+					prime[i] = false;
+				}
+			}
+		}
+
+		for 
 		
 	}
 
