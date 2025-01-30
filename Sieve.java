9018de8ff749c032f2bb9e12b986d651f53c2919
Kushagra Dwivedi <kishudwivedi30@gmail.com>
2025-01-31 02:03:45 +0530

    simple sieve

---
+++ Sieve.java
@@ -0,0 +1,13 @@
+public class Sieve {
+	public static void simpleSieve(int limit){
+			boolean[] prime = new boolean[limit + 1];
+	
+
+
+		for (int i =2; i<=limit; i++) {
+			prime[i] = true;
+		}
+		
+	}
+
+}
